1a6e60cf83e64db60fbcdc5c59ba59b77e3450ec
Cauã Rossi <cauarossi007@gmail.com>
2023-05-02 21:26:11 -0300

    ARQUIVO GRAVAR

---
+++ BACK-END/IOT/teste/gravar.php
@@ -0,0 +1,36 @@
+
+<?php
+
+    $redis = new Redis();
+    $redis->connect('127.0.0.1'); //conecta na porta padao
+    
+    if($redis){
+      echo "conectado <br>";
+    }else{
+      echo "erro ao conectar";
+    }
+
+    ===========================================================
+    /*Passar dados para o banco em 5 seg na váriavel vento*/ 
+  
+    if(isset($_GET['vento'])){
+
+      $i=0;
+
+      while (TRUE) {
+        $vento = $i;
+        $redis->set('vento', $vento); 
+        $i++;
+        sleep(5);
+      }
+      
+        
+    }
+
+
+
+
+?>
+
+
+</html>
