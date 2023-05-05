8ee9933d1a7006f7c70399f6285eb4cbe6bc8620
Emerson-Sotero <emersonsotero121@gmail.com>
2023-05-05 02:25:54 -0300

    refatoração ProgressBar

--- FRONT-END/Circular Progress Bar IOT/script.js
+++ FRONT-END/Circular Progress Bar IOT/script.js
@@ -1,109 +1,25 @@
-let progressBarTensao = document.querySelector(".circular-progress-tensao");
-let valueContainerTensao = document.querySelector(".value-container-tensao");
+class CircularProgressBar{
 
-let progressBarVento = document.querySelector(".circular-progress-vento");
-let valueContainerVento = document.querySelector(".value-container-vento");
-
-let progressBarYaw = document.querySelector(".circular-progress-yaw");
-let valueContainerYaw = document.querySelector(".value-container-yaw");
-
-let progressBarPitch = document.querySelector(".circular-progress-pitch");
-let valueContainerPitch = document.querySelector(".value-container-pitch");
-
-let progressBarRpm = document.querySelector(".circular-progress-rpm");
-let valueContainerRpm = document.querySelector(".value-container-rpm");
-
-/*=========================================================*/
-
-let progressValueTensao = 0;
-let progressEndValueTensao = 100;
-let speedTensao = 5;
-
-
-let progressTensao = setInterval(() => {
-  progressValueTensao++;
-  valueContainerTensao.textContent = `${progressValueTensao}%`;
-  progressBarTensao.style.background = `conic-gradient(
-      #4d5bf9 ${progressValueTensao * 3.6}deg,
-      #cadcff ${progressValueTensao * 3.6}deg
-  )`;
-  if (progressValueTensao == progressEndValueTensao) {
-    clearInterval(progressTensao);
+  constructor(valorMax, medida,idProgressBar, idValueProgress){
+    this.medida = medida;
+    this.graus = 360 / valorMax;
+    this.idProgressBar = document.getElementById(idProgressBar);
+    this.idValueProgress = document.getElementById(idValueProgress);
   }
-}, speedTensao);
 
-/*=========================================================*/
-
-let progressValueVento = 0;
-let progressEndValueVento = 80;
-let speedVento = 5;
-
-
-let progressVento = setInterval(() => {
-  progressValueVento++;
-  valueContainerVento.textContent = `${progressValueVento}%`;
-  progressBarVento.style.background = `conic-gradient(
-      #4d5bf9 ${progressValueVento * 3.6}deg,
-      #cadcff ${progressValueVento * 3.6}deg
+  setProgresso(progress){
+    this.idValueProgress.textContent = progress + this.medida;
+    this.idProgressBar.style.background = `conic-gradient(
+      #4d5bf9 ${progress * this.graus}deg,
+      #cadcff ${progress * this.graus}deg
   )`;
-  if (progressValueVento == progressEndValueVento) {
-    clearInterval(progressVento);
   }
-}, speedVento);
 
-/*=========================================================*/
-
-let progressValueYaw = 0;
-let progressEndValueYaw = 60;
-let speedYaw = 5;
+}
 
 
-let progressYaw = setInterval(() => {
-  progressValueYaw++;
-  valueContainerYaw.textContent = `${progressValueYaw}%`;
-  progressBarYaw.style.background = `conic-gradient(
-      #4d5bf9 ${progressValueYaw * 3.6}deg,
-      #cadcff ${progressValueYaw * 3.6}deg
-  )`;
-  if (progressValueYaw == progressEndValueYaw) {
-    clearInterval(progressYaw);
-  }
-}, speedYaw);
-
-/*=========================================================*/
-
-let progressValuePitch = 0;
-let progressEndValuePitch = 40;
-let speedPitch = 5;
-
-
-let progressPitch = setInterval(() => {
-  progressValuePitch++;
-  valueContainerPitch.textContent = `${progressValuePitch}%`;
-  progressBarPitch.style.background = `conic-gradient(
-      #4d5bf9 ${progressValuePitch * 3.6}deg,
-      #cadcff ${progressValuePitch * 3.6}deg
-  )`;
-  if (progressValuePitch == progressEndValuePitch) {
-    clearInterval(progressPitch);
-  }
-}, speedPitch);
-
-/*=========================================================*/
-
-let progressValueRpm = 0;
-let progressEndValueRpm = 20;
-let speedRpm = 5;
-
-
-let progressRpm = setInterval(() => {
-  progressValueRpm++;
-  valueContainerRpm.textContent = `${progressValueRpm}%`;
-  progressBarRpm.style.background = `conic-gradient(
-      #4d5bf9 ${progressValueRpm * 3.6}deg,
-      #cadcff ${progressValueRpm * 3.6}deg
-  )`;
-  if (progressValueRpm == progressEndValueRpm) {
-    clearInterval(progressRpm);
-  }
-}, speedRpm);
+let circularProgressBarTensao = new CircularProgressBar(12, "V","circular-progress-tensao","value-container-tensao");
+let circularProgressBarVento = new CircularProgressBar(12, "km","circular-progress-vento","value-container-vento");
+let circularProgressBarYAW = new CircularProgressBar(12, "º","circular-progress-yaw","value-container-yaw");
+let circularProgressBarRPM = new CircularProgressBar(12, "rpm","circular-progress-rpm","value-container-rpm");
+let circularProgressBarPitch = new CircularProgressBar(12, "º","circular-progress-pitch","value-container-pitch");
