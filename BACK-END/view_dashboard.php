38b6a198a1640840f362994c82a0710e1e4cb5c0
Emerson Sotero <emersonsotero121@gmail.com>
2023-05-05 20:04:45 -0300

    system login register

---
+++ BACK-END/view_dashboard.php
@@ -0,0 +1,23 @@
+<?php
+require_once('../back-end/autenticate.php');
+function dashboard(){
+    if (verificar_autenticacao()) {
+            echo "
+            <div class='cards-container'>
+            <div class='cards'>12V<br>Tensão</div>         
+            <div class='cards'>30 Km/h<br>Vento</div>         
+            <div class='cards'>250º<br> YAW</div>         
+            <div class='cards'>60<br>RPM</div>         
+            <div class='cards'>85º<br>Ataque</div>   
+        </div>
+            ";
+        } else {
+            echo"
+                <div class='cards-container'>
+                    <p>Faça login para ter acesso!</p>
+                </div>
+            ";
+        }
+}
+    
+                 
