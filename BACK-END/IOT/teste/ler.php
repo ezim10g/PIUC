09e77f10ed2d86cdd9c82f5c65e1c5ea55b87333
ezim10g <ezim10g@gmail.com>
2023-05-05 19:49:33 -0300

    teste php

--- BACK-END/IOT/teste/ler.php
+++ BACK-END/IOT/teste/ler.php
@@ -1,26 +1,12 @@
 
+
 <?php
+            $redis = new Redis();
+               $redis->connect('127.0.0.1');            
 
-    $redis = new Redis();
-    $redis->connect('127.0.0.1'); //conecta na porta padao
-    
-    if($redis){
-      echo "conectado <br>";
-    }else{
-      echo "erro ao conectar";
-    }
+            if(isset($_GET['mostrar'])){
+                echo $redis->get('vento');
+            }
 
-//ler dados do banco de dados
-
-    if(isset($_GET['ler'])){
-      echo "ler ok";
-      while(true){
-        echo $redis->get('vento'); //buca o valor da chave nome
-        sleep(1);
-      }
-
-    }
-    
-?>
-
+?>   
 
