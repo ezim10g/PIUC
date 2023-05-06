cf04db7b7cd660c5046bc3732ed0e15d1d76916d
ezim10g <ezim10g@gmail.com>
2023-05-06 00:40:17 -0300

    gravar atualizado

--- BACK-END/IOT/teste/gravar.php
+++ BACK-END/IOT/teste/gravar.php
@@ -1,36 +1,47 @@
 
-
 <?php
 
     $redis = new Redis();
     $redis->connect('127.0.0.1'); //conecta na porta padao
     
-    if($redis){
-      echo "conectado <br>";
-    }else{
-      echo "erro ao conectar";
-    }
 
-       /*Passar dados para o banco em 5 seg na váriavel vento*/ 
-  
     if(isset($_GET['vento'])){
 
-      $i=0;
-
-      while (TRUE) {
-        $vento = $i;
-        $redis->set('vento', $vento, 60); 
-        $i++;
-        echo $vento ."<br>";
-        sleep(5);
-
-
-        if(isset($_GET['parar'])){
-                exit();
-        }
-
-      }
-
-
+        $vento = $_GET['vento'];
+        $redis->set('vento', $vento, 10); //cria uma chave que expira em 10 seg
+        
     }
 
+    if(isset($_GET['tensao'])){
+
+        $tensao = $_GET['tensao'];
+        $redis->set('tensao', $tensao, 10); //cria uma chave que expira em 10 seg
+        
+    }
+
+    if(isset($_GET['rpm'])){
+
+        $rpm = $_GET['rpm'];
+        $redis->set('rpm', $rpm, 10); //cria uma chave que expira em 10 seg
+        
+    }
+
+    if(isset($_GET['yaw'])){
+
+        $yaw = $_GET['yaw'];
+        $redis->set('yaw', $yaw, 10); //cria uma chave que expira em 10 seg
+        
+    }
+
+  
+    if(isset($_GET['pitch'])){
+
+        $pich = $_GET['pitch'];
+        $redis->set('pitch', $ataque, 10); //cria uma chave que expira em 10 seg
+        
+    }
+  
+?>
+
+
+</html>
