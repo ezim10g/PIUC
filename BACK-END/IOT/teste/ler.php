fee3c68257faf9fc16eca82cfe49035821dd6276
ezim10g <ezim10g@gmail.com>
2023-05-06 01:57:49 -0300

    teste iot ok

--- BACK-END/IOT/teste/ler.php
+++ BACK-END/IOT/teste/ler.php
@@ -6,9 +6,12 @@
 
             if(isset($_GET['mostrar'])){
                 $vento = $redis->get('vento');                        
+                $tensao = $redis->get('tensao');                        
                 $rpm = $redis->get('rpm');
+                $yaw = $redis->get('yaw');
+                $pitch = $redis->get('pitch');
 
-                $data = array('vento' => $vento, 'tensao' => $vento ,'rpm' => $rpm, 'yaw' => $yaw , 'pitch' => $pitch);
+                $data = array('vento' => $vento, 'tensao' => $tensao ,'rpm' => $rpm, 'yaw' => $yaw , 'pitch' => $pitch);
 
                 $json = json_encode($data);
                 
