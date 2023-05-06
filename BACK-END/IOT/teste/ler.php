417adcc05d24de3834429d897b7c2e37523896b8
ezim10g <ezim10g@gmail.com>
2023-05-06 00:33:27 -0300

    metodo gat com json adicionado

--- BACK-END/IOT/teste/ler.php
+++ BACK-END/IOT/teste/ler.php
@@ -5,9 +5,16 @@
                $redis->connect('127.0.0.1');            
 
             if(isset($_GET['mostrar'])){
-                echo $redis->get('vento');
-                echo "|";
-                echo $redis->get('rpm');
+                $vento = $redis->get('vento');                        
+                $rpm = $redis->get('rpm');
+
+                $data = array('vento' => $vento, 'rpm' => $rpm);
+
+                $json = json_encode($data);
+                
+                header('Content-Type: application/json');
+               
+                echo $json;
 
             }
 
