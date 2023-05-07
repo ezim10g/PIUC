2b146149b1dd0c8033fb299ab6d78902090a6094
Emerson-Sotero <emersonsotero121@gmail.com>
2023-05-07 14:27:13 -0300

    animação submenu

--- FRONT-END/profile.js
+++ FRONT-END/profile.js
@@ -1,9 +1,16 @@
 const perfil = document.getElementById("profileBtn");
 const submenu = document.getElementById("submenuperfil");
-
+let cont = 0;
 if(perfil){
     perfil.addEventListener('click', () =>{
-        submenu.classList.add("submenuactive")
+        if(cont == 0){
+            submenu.classList.add("submenuactive");
+            cont = 1;
+        }else{
+            submenu.classList.remove("submenuactive");
+            cont = 0;
+        }
+        
         
 
     });
