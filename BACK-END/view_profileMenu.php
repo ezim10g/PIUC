6bd6cda7199a3c1ca91cada0d1e405ad90e198b3
Emerson-Sotero <emersonsotero121@gmail.com>
2023-05-07 14:38:48 -0300

    mudanças no codigo de submenu de perfil

--- BACK-END/view_profileMenu.php
+++ BACK-END/view_profileMenu.php
@@ -9,14 +9,14 @@
         <div class="sub-menu-profile" id="submenuperfil">
             <div class="user-info">
             <img src="../FRONT-END/assets/profile.jpg" class="profile-img" alt="imagem de perfil">
-            <h2><?php echo $_SESSION['nomeUsuario'];?></h2>
+            <h2 ><?php echo $_SESSION['nomeUsuario'];?></h2>
             </div>
 
             <div class="sub-menu-options">
                 <ul>
-                    <li><a href="#">Perfil</a></li>
-                    <li><a href="#">Respostas</a></li>
-                    <li><a href="#">Sair</a></li>
+                    <li><a class="itens-submenu" href="#">Perfil</a></li>
+                    <li><a class="itens-submenu" href="#">Respostas</a></li>
+                    <li><a class="itens-submenu" href="#">Sair</a></li>
                 </ul>
             </div>
         </div>
