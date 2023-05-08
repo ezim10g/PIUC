450c53845299b7a2c83fa91c9dd5e902a06a90ba
ezim10g <ezim10g@gmail.com>
2023-05-07 21:45:37 -0300

    siglas km , rpm

--- FRONT-END/scriptCircular.js
+++ FRONT-END/scriptCircular.js
@@ -21,9 +21,9 @@ class CircularProgressBar{
 
 
 let circularProgressBarTensao = new CircularProgressBar(12, " V","circular-progress-tensao","value-container-tensao");
-let circularProgressBarVento = new CircularProgressBar(100, " km","circular-progress-vento","value-container-vento");
+let circularProgressBarVento = new CircularProgressBar(100, " Km","circular-progress-vento","value-container-vento");
 let circularProgressBarYAW = new CircularProgressBar(360, "º","circular-progress-yaw","value-container-yaw");
-let circularProgressBarRPM = new CircularProgressBar(800, " rpm","circular-progress-rpm","value-container-rpm");
+let circularProgressBarRPM = new CircularProgressBar(800, " Rpm","circular-progress-rpm","value-container-rpm");
 let circularProgressBarPitch = new CircularProgressBar(90, "º","circular-progress-pitch","value-container-pitch");
 
 
