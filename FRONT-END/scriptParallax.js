bc04142d70f8c03e92e4579c102364d7a1888761
Davyd Cristiano <davyd-davy@hotmail.com>
2023-05-09 21:25:45 +1300

    finalmente_desgraça

--- FRONT-END/scriptParallax.js
+++ FRONT-END/scriptParallax.js
@@ -1,7 +1,6 @@
 let bg = document.getElementById("bg");
 let bgfundo = document.getElementById("bgfundo");
 let bgwind = document.getElementById("bgwind");
-let text = document.getElementById("text");
 
 window.addEventListener('scroll', function() {
     var value = window.scrollY;
@@ -9,4 +8,25 @@ window.addEventListener('scroll', function() {
     bg.style.top = value * 0.5 + 'px';
     bgfundo.style.right = - value * 0.5 + 'px';
     bgwind.style.left = value * 0.15 + 'px';
+
 });
+
+function animateLeft(obj, from, to){
+    if(from >= to){         
+        /*obj.style.visibility = 'hidden';*/
+        return;  
+    }
+    else {
+        var box = obj;
+        box.style.marginLeft = from + "px";
+        setTimeout(function(){
+            animateLeft(obj, from + 1, to);
+        }, 25) 
+    }
+ }
+ 
+ function animateMe() {
+
+ animateLeft(document.getElementById('bgwind'), 100, 900);
+
+ }
