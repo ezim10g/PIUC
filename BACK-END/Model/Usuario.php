cd80f2c40cecbb862bb19a33678b9ac9739e1212
Emerson Sotero <emersonsotero121@gmail.com>
2023-05-09 22:13:22 -0300

    função de registrarusuario adcionada a classe Usuario

--- BACK-END/Model/Usuario.php
+++ BACK-END/Model/Usuario.php
@@ -1,20 +1,27 @@
 <?php
 include '../util/validateData.php';
+include '../DAO/UsuarioDAO.php';
 class Usuario{
 
+    public $nome;
+    public $email;
+
     function ValidarNome($nome){
-        ValidateData($nome);
-        if(strlen($usuario) > 25){
+        $nome = Validate($nome);
+        if(strlen($nome) < 25){
+            $this->$nome = $nome;
             return true;
         }else{
             return false;
         }
+
     }
 
     function ValidarEmail($email){
-        ValidateData($email);
+        $email = Validate($email);
         $regex = "/^\w+([\.-]?\w+)*@\w+([\.-]?w+)*(\.\w{2,3})+$/";
         if(preg_match($regex, $email) == 1){
+            $this->$email = $email;
             return true;
         }else{
             return false;
@@ -35,10 +42,20 @@ class Usuario{
     }
 
     function RegistrarUsuario($nome,$email,$senha){
-
+        if($this->ValidarNome($nome) && $this->ValidarEmail($email)){
+            $senha = $this->CodificarSenha($senha);
+            $usuarioDAO = new UsuarioDAO();
+            $usuarioDAO->setUsuario($nome,$email,$senha);
+            return true;
+        }else{
+            return false;
+        }
     }
 
     function LogarUsuario($email,$senha){
-        
+
     }
 }
+
+$user = new Usuario();
+$user->RegistrarUsuario('eminhs','sotero@gmail.com','123456');
