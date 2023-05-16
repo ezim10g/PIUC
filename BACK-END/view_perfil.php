726038133dc2007eb10b8678bb48916fa5a6e407
Emerson Sotero <emersonsotero121@gmail.com>
2023-05-16 20:17:24 -0300

    atualizando perfil

--- BACK-END/view_perfil.php
+++ BACK-END/view_perfil.php
@@ -7,8 +7,9 @@
                 <div class="titulo-div">
                         <h2>Perfil</h2>
                 </div>
+        <div class="Perfil">
 
-                <div id="fotoPerfil">
+        <div id="fotoPerfil">
                         <img src="../FRONT-END/assets/<?php echo $_SESSION['fotoPerfil']; ?>" class="img-perfil"
                                 id="imgPerfil" alt="imagem de perfil">
                         <img src="../FRONT-END/assets/cam-icon.webp" alt="" id="cam-icon">
@@ -41,7 +42,23 @@
                                 }
                                 ; ?>
                         </p>
-                        <button>Editar Perfil</button><button>Excluir Conta</button>
+                        <div class="buttons"><button>Editar Perfil</button><button>Excluir Conta</button></div>
+                        
+                </div>
+        </div>
+                <div id="imgSelect"class="choose-imagens">
+                        <div class="titulo"><h2>escolha sua nova foto de perfil</h2></div>
+                        <form action="../chooseIMG.php">
+
+                        <div class="fotos-perfil">
+                                <img src="../FRONT-END/assets/profile.jpg" alt="">
+                                <img src="../FRONT-END/assets/profile2.png" alt="">
+                                <img src="../FRONT-END/assets/profile3.jpg" alt="">
+                        </div>
+                        <input type="submit" value="Enviar" name="enviarIMG">
+                        </form>
+                        
+                        
                 </div>
 
 
