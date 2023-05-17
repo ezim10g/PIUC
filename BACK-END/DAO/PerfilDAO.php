28613611f9cee2dc4d6794624bad96f87596e9c6
Emerson Sotero <emersonsotero121@gmail.com>
2023-05-16 21:53:38 -0300

    att perfil

--- BACK-END/DAO/PerfilDAO.php
+++ BACK-END/DAO/PerfilDAO.php
@@ -9,6 +9,14 @@
         $stmt->execute();
     }
 
+    function setFotoPerfil($fotoPerfil, $id){
+        $sql = "UPDATE perfil SET fotoPerfil = :fotoPerfil WHERE idUsuario = :id";
+        $stmt = PrepareSQL($sql);
+        $stmt-> bindParam(':fotoPerfil', $fotoPerfil,PDO::PARAM_STR);
+        $stmt-> bindParam(':id', $id,PDO::PARAM_INT);
+        $stmt-> execute();
+    }
+
 
  }
 
