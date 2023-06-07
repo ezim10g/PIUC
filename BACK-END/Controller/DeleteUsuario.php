8b92a378cf590aee2a6a7f675c1c1280cf601057
Emerson Sotero <emersonsotero121@gmail.com>
2023-06-07 20:05:39 -0300

    controllerdelete

--- BACK-END/Controller/DeleteUsuario.php
+++ BACK-END/Controller/DeleteUsuario.php
@@ -1,18 +1,23 @@
 <?php
 session_start();
-require_once (dirname(__FILE__) ."../AutenticateController.php");
+require_once(dirname(__FILE__) . "../AutenticateController.php");
 
-include (dirname(__FILE__) . "../../Model/Usuario.php");
+include(dirname(__FILE__) . "../../Model/Usuario.php");
 
 $usuario = new Usuario();
+if (isset($_POST)) {
+   if (verificar_autenticacao()) {
+      $usuario->deletarUsuario($_SESSION['id']);
+      session_destroy();
+      header("location: ../../FRONT-END/index.php?message= Usuário excluido com sucesso!");
+      exit();
 
-if(verificar_autenticacao()){
-   $usuario->deletarUsuario($_SESSION['id']); 
-   session_destroy();
-   header("location: ../../FRONT-END/index.php?message= Usuário excluido com sucesso!");
-   exit();  
-   
-}else{
-    header("location: ../../FRONT-END/index.php?Erro= erro ao exluir usuário!");
-   exit();
+   } else {
+      header("location: ../../FRONT-END/index.php?Erro= erro ao exluir usuário!");
+      exit();
+   }
+
 }
+
+header("location: ../../FRONT-END/index.php");
+exit();
